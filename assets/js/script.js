10c9db5eed64809ddd644cf99c41fa161e5f0d7c
HannahMidd <hmiddleton1017@gmail.com>
2023-06-08 16:18:56 -0400

    adding working API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,128 +1,132 @@
+// Variables for the workout section
 var bodyPart = [
-  "waist",
-  "upper legs",
-  "back",
-  "lower legs",
-  "chest",
-  "upper arms",
-  "shoulders",
-  "cardio",
-  "lower arms",
-  ,
+    'waist',
+    'upper legs',
+    'back',
+    'lower legs',
+    'chest',
+    'upper arms',
+    'shoulders',
+    'cardio',
+    'lower arms',
+    ,
 ];
-
 var equipment = [
-  "barbell",
-  "rope",
-  "body-weight",
-  "assisted",
-  "cable",
-  "leverage machine",
-  "stability ball",
+    'barbell',
+    'rope',
+    'body-weight',
+    'assisted',
+    'cable',
+    'leverage machine',
+    'stability ball',
 ];
-
 // var difficulty = [
 // "beginner",
 // "intermediate",
 // "expert",
 // ];
-
-const apiKey = "b5a440c4d4msh54112a87c5e9245p18176fjsnfa9cc2dad087";
+const apiKey = 'b5a440c4d4msh54112a87c5e9245p18176fjsnfa9cc2dad087';
 const options = {
-  headers: {
-    "X-RapidAPI-Key": "b5a440c4d4msh54112a87c5e9245p18176fjsnfa9cc2dad087",
-    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-  },
+    headers: {
+        'X-RapidAPI-Key': 'b5a440c4d4msh54112a87c5e9245p18176fjsnfa9cc2dad087',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    },
 };
 document
-  .querySelector("#workoutForm")
-  .addEventListener("submit", function (sub) {
-    sub.preventDefault();
-    let bodyP = document.getElementById("bodyPart").value.toLowerCase();
-    let equip = document.getElementById("equipment").value.toLowerCase();
-    // getWorkOut(bodyPart, equipment);
-    // getgif(bodyPart, equipment);
-    // console.log(bodyP);
-    // console.log(equip);
-    getWorkout(bodyP, equip);
-  });
-
+    .querySelector('#workoutForm')
+    .addEventListener('submit', function (sub) {
+        sub.preventDefault();
+        let bodyP = document.getElementById('bodyPart').value.toLowerCase();
+        let equip = document.getElementById('equipment').value.toLowerCase();
+        // getWorkOut(bodyPart, equipment);
+        // getgif(bodyPart, equipment);
+        // console.log(bodyP);
+        // console.log(equip);
+        getWorkout(bodyP, equip);
+    });
 function getWorkout(bodyP, equip) {
-  const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyP}`;
-  var gifUrl = fetch(url, options)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].equipment === equip) {
-          // other stuff to do
-          /* 
+    const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyP}`;
+    var gifUrl = fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].equipment === equip) {
+                    // other stuff to do
+                    /*
             store values in an array
             pick random value from that array
             display random value to user
           */
-          console.log(data[i]);
-        }
-      }
-      // renderItems(name, data);
-      // renderItems(gifUrl, data),
-      //   (document.getElementById("workout").textContent = workout);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+                    console.log(data[i]);
+                }
+            }
+            // renderItems(name, data);
+            // renderItems(gifUrl, data),
+            //   (document.getElementById("workout").textContent = workout);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
-
 // --------------END WORKOUT SECTION------------------------------------
 // --------------EVERYTHING BELOW IS RECIPE BASED------------------------
-
-var dietSelect = document.getElementById("diet");
-var cuisineSelect = document.getElementById("cuisine");
-var intolerancesSelect = document.getElementById("intolerances");
-
+var dietSelect = document.getElementById('diet');
+var cuisineSelect = document.getElementById('cuisine');
+var intolerancesSelect = document.getElementById('intolerances');
 var apiReciKey = "43050d7446924af49c324ae43ecfbafa";
-var RecipeEndpoint = `https://api.spoonacular.com/recipes/complexSearch?diet=${dietSelect.value}&cuisine=${cuisineSelect.value}&intolerances=${intolerancesSelect.value}&number=15&apiKey=${apiReciKey}`;
-
-var recipeButton = document.getElementById("randRecipes");
-var recipeSection = document.querySelector(".unhideRecipeInfo");
-var recipeTitle = document.getElementById("recipeTitle");
-var recipeImage = document.getElementById("recipeImage");
-
-recipeButton.addEventListener("click", function () {
-  fetch(RecipeEndpoint)
-    .then((response) => {
-      console.log("This recipe API works!");
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data.results[0].title);
-      recipeTitle.textContent = data.results[0].title;
-      // Change Title class to make visible
-      recipeTitle.classList.remove(".unhideRecipeInfo");
-      recipeTitle.classList.add(".showRecipeInfo");
-      // Update Image and change class to make visible
-      recipeImage.src = data.results[0].image;
-      recipeSection.style.display = "block";
-      recipeImage.classList.remove(".unhideRecipeInfo"); 
-      recipeImage.classList.add(".showRecipeInfo"); 
-    })
-    .catch((error) => {
-      console.error("Recipe Error:", error.message);
-    });
+var recipeButton = document.getElementById('randRecipes');
+var recipeSection = document.querySelector('.unhideRecipeInfo');
+var recipeTitle = document.getElementById('recipeTitle');
+var recipeImage = document.getElementById('recipeImage');
+recipeButton.addEventListener('click', function () {
+    var recipeEndpoint = `https://api.spoonacular.com/recipes/complexSearch?diet=${dietSelect.value}&cuisine=${cuisineSelect.value}&intolerances=${intolerancesSelect.value}&number=1&apiKey=${apiReciKey}`;
+    fetch(recipeEndpoint)
+        .then((response) => {
+            console.log('This recipe API works!');
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data.results[0]);
+            recipeTitle.textContent = data.results[0].title;
+            // Change Title class to make visible
+            recipeTitle.classList.remove('.unhideRecipeInfo');
+            recipeTitle.classList.add('.showRecipeInfo');
+            // Update Image and change class to make visible
+            recipeImage.src = data.results[0].image;
+            recipeSection.style.display = 'block';
+            recipeImage.classList.remove('.unhideRecipeInfo');
+            recipeImage.classList.add('.showRecipeInfo');
+            var singleRecipeURL = `https://api.spoonacular.com/recipes/${data.results[0].id}/information?apiKey=${apiReciKey}`;
+            fetch(singleRecipeURL)
+                .then((response) => {
+                    console.log('This single recipe API works!');
+                    if (!response.ok) {
+                        throw new Error(`Error: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                });
+        })
+        .catch((error) => {
+            console.error('Recipe Error:', error.message);
+        });
 });
 
-// document
-//   .querySelector("#workoutForm")
-//   .addEventListener("submit", function (sub) {
-//     sub.preventDefault();
-//     let bodyP = document.getElementById("bodyPart").value.toLowerCase();
-//     let equip = document.getElementById("equipment").value.toLowerCase();
-//     // getWorkOut(bodyPart, equipment);
-//     // getgif(bodyPart, equipment);
-//     // console.log(bodyP);
-//     // console.log(equip);
-//     getWorkout(bodyP, equip);
-//   });
+
+
+
+
+
+
+
+
+
+
+
+
