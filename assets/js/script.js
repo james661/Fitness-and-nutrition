62714154fa2cb6e5c27c303bc5c1c9c7540e5e8b
earnest-dawn <edawn2000@gmail.com>
2023-06-08 16:22:57 -0700

    I Fixed the pagegit add -A someone check and see if its good but its fully functional on my end. ps i just noticed i was on the darn main  branch.. i never went back to my branch after jeff had me switch to it :( but hopefully everything is good.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,45 @@
-// Variables for the workout section
 var bodyPart = [
-    'waist',
-    'upper legs',
-    'back',
-    'lower legs',
-    'chest',
-    'upper arms',
-    'shoulders',
-    'cardio',
-    'lower arms',
-    ,
+  "waist",
+  "upper legs",
+  "back",
+  "lower legs",
+  "chest",
+  "upper arms",
+  "shoulders",
+  "cardio",
+  "lower arms",
 ];
+
 var equipment = [
-    'barbell',
-    'rope',
-    'body-weight',
-    'assisted',
-    'cable',
-    'leverage machine',
-    'stability ball',
+  "barbell",
+  "rope",
+  "body-weight",
+  "assisted",
+  "cable",
+  "leverage machine",
+  "stability ball",
 ];
 
 let randomwrkout = [];
 
-// var difficulty = [
-// "beginner",
-// "intermediate",
-// "expert",
-// ];
-const apiKey = 'b5a440c4d4msh54112a87c5e9245p18176fjsnfa9cc2dad087';
+const apiKey = "b5a440c4d4msh54112a87c5e9245p18176fjsnfa9cc2dad087";
 const options = {
-
   headers: {
     "X-RapidAPI-Key": "a410389ea3msha482df8372ade2bp19d872jsn0909b3343fdd",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
-}; 
-
+};
 
 document
-    .querySelector('#workoutForm')
-    .addEventListener('submit', function (sub) {
-        sub.preventDefault();
-        let bodyP = document.getElementById('bodyPart').value.toLowerCase();
-        let equip = document.getElementById('equipment').value.toLowerCase();
-        // getWorkOut(bodyPart, equipment);
-        // getgif(bodyPart, equipment);
-        // console.log(bodyP);
-        // console.log(equip);
-        getWorkout(bodyP, equip);
-    });
-function getWorkout(bodyP, equip) {
+  .querySelector("#workoutForm")
+  .addEventListener("submit", function (sub) {
+    sub.preventDefault();
+    let bodyP = document.getElementById("bodyPart").value.toLowerCase();
+    let equip = document.getElementById("equipment").value.toLowerCase();
+    getWorkout(bodyP, equip);
+  });
 
+function getWorkout(bodyP, equip) {
   const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyP}`;
   var gifUrl = fetch(url, options)
     .then((response) => response.json())
@@ -74,64 +62,67 @@ function getWorkout(bodyP, equip) {
       let wrkoutName = document.createElement("h1");
       wrkoutContainer.setAttribute("id", "wrkoutContainer");
       wrkoutName.textContent = randomwrkout[index].name;
+      let wrkoutGif = document.createElement("h2");
 
+      var img = document.createElement("img");
+      img.src = randomwrkout[index].gifUrl;
+      img.alt = "Exercise GIF";
+
+      wrkoutContainer.append(wrkoutGif);
+      wrkoutGif.append(img);
       wrkoutContainer.append(wrkoutName);
       workoutR.append(wrkoutContainer);
-
-      // renderItems(name, data);
-      // renderItems(gifUrl, data),
-      //   (document.getElementById("workout").textContent = workout);
     })
     .catch((error) => {
       console.error(error);
     });
-
 }
+
 // --------------END WORKOUT SECTION------------------------------------
 // --------------EVERYTHING BELOW IS RECIPE BASED------------------------
-var dietSelect = document.getElementById('diet');
-var cuisineSelect = document.getElementById('cuisine');
-var intolerancesSelect = document.getElementById('intolerances');
+var dietSelect = document.getElementById("diet");
+var cuisineSelect = document.getElementById("cuisine");
+var intolerancesSelect = document.getElementById("intolerances");
 var apiReciKey = "43050d7446924af49c324ae43ecfbafa";
-var recipeButton = document.getElementById('randRecipes');
-var recipeSection = document.querySelector('.unhideRecipeInfo');
-var recipeTitle = document.getElementById('recipeTitle');
-var recipeImage = document.getElementById('recipeImage');
-recipeButton.addEventListener('click', function () {
-    var recipeEndpoint = `https://api.spoonacular.com/recipes/complexSearch?diet=${dietSelect.value}&cuisine=${cuisineSelect.value}&intolerances=${intolerancesSelect.value}&number=1&apiKey=${apiReciKey}`;
-    fetch(recipeEndpoint)
+var recipeButton = document.getElementById("randRecipes");
+var recipeSection = document.querySelector(".unhideRecipeInfo");
+var recipeTitle = document.getElementById("recipeTitle");
+var recipeImage = document.getElementById("recipeImage");
+recipeButton.addEventListener("click", function () {
+  var recipeEndpoint = `https://api.spoonacular.com/recipes/complexSearch?diet=${dietSelect.value}&cuisine=${cuisineSelect.value}&intolerances=${intolerancesSelect.value}&number=1&apiKey=${apiReciKey}`;
+  fetch(recipeEndpoint)
+    .then((response) => {
+      console.log("This recipe API works!");
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.results[0]);
+      recipeTitle.textContent = data.results[0].title;
+      // Change Title class to make visible
+      recipeTitle.classList.remove(".unhideRecipeInfo");
+      recipeTitle.classList.add(".showRecipeInfo");
+      // Update Image and change class to make visible
+      recipeImage.src = data.results[0].image;
+      recipeSection.style.display = "block";
+      recipeImage.classList.remove(".unhideRecipeInfo");
+      recipeImage.classList.add(".showRecipeInfo");
+      var singleRecipeURL = `https://api.spoonacular.com/recipes/${data.results[0].id}/information?apiKey=${apiReciKey}`;
+      fetch(singleRecipeURL)
         .then((response) => {
-            console.log('This recipe API works!');
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-            return response.json();
+          console.log("This single recipe API works!");
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+          }
+          return response.json();
         })
         .then((data) => {
-            console.log(data.results[0]);
-            recipeTitle.textContent = data.results[0].title;
-            // Change Title class to make visible
-            recipeTitle.classList.remove('.unhideRecipeInfo');
-            recipeTitle.classList.add('.showRecipeInfo');
-            // Update Image and change class to make visible
-            recipeImage.src = data.results[0].image;
-            recipeSection.style.display = 'block';
-            recipeImage.classList.remove('.unhideRecipeInfo');
-            recipeImage.classList.add('.showRecipeInfo');
-            var singleRecipeURL = `https://api.spoonacular.com/recipes/${data.results[0].id}/information?apiKey=${apiReciKey}`;
-            fetch(singleRecipeURL)
-                .then((response) => {
-                    console.log('This single recipe API works!');
-                    if (!response.ok) {
-                        throw new Error(`Error: ${response.status}`);
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    console.log(data);
-                });
-        })
-        .catch((error) => {
-            console.error('Recipe Error:', error.message);
+          console.log(data);
         });
+    })
+    .catch((error) => {
+      console.error("Recipe Error:", error.message);
+    });
 });
